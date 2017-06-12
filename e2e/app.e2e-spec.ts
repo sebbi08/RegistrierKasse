a5d19c622da0bf8c4ff1dbd9584716c65a06e533
Angular CLI <angular-cli@angular.io>
2017-06-12 22:49:29 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { RegistrierKassePage } from './app.po';
+
+describe('registrier-kasse App', () => {
+  let page: RegistrierKassePage;
+
+  beforeEach(() => {
+    page = new RegistrierKassePage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
