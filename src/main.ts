46c9d410bdb3d558dea647b74ec8d711f4199dbc
Sebastian Mahr <sebastian@Sebastian.localdomain>
2018-04-07 11:22:12 +0200

    init

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+Vue.config.productionTip = false;
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
